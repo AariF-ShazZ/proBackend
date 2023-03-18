70c9ec1d2efd450b4d969f5137503b14bc771bce
Aarif-shazZ <shazaarif0@gmail.com>
2023-03-18 11:27:36 +0530

    done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express  = require("express")
 require("dotenv").config()
 const cors = require("cors")
-const {connection}   = require("./configs/db")
+const connect   = require("./configs/db")
 const {userRoutes} = require("./routes/User.route")
 const {productRoutes} = require("./routes/Product.route")
 
@@ -17,12 +17,7 @@ app.get("/",(req,res) => {
 app.use("/user",userRoutes)
 app.use("/product",productRoutes)
 
-app.listen(process.env.port,async(req,res) => {
-    try{
-         await connection
-         console.log(`Server Run Successfully at port number ${process.env.port}`);
-    }catch(err){
-        console.log(" Trouble connecting to the DB");
-        res.send("Trouble connecting to the DB")
-    }
+app.listen(process.env.port,async ()=>{
+    await connect()
+    console.log("server is running on port 8080")
 })
